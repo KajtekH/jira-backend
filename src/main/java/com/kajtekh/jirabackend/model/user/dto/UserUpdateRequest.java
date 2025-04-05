442155268d809db1bd360eb3a9765b4db82164fd
KajtekH <kajhulboj@wp.pl>
2025-04-05 16:37:39 +0200

    Code cleanup

--- src/main/java/com/kajtekh/jirabackend/model/user/dto/UserUpdateRequest.java
+++ src/main/java/com/kajtekh/jirabackend/model/user/dto/UserUpdateRequest.java
@@ -1,6 +1,4 @@
 package com.kajtekh.jirabackend.model.user.dto;
 
-import com.kajtekh.jirabackend.model.user.User;
-
 public record UserUpdateRequest(String username, String email, String firstName, String lastName) {
 }
